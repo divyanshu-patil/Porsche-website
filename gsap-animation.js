a1349d06e88408d1b9118c59a9e71d18b0758bc0
Bhavesh-More <bhaveshmmore2006@gmail.com>
2024-07-04 02:57:31 +0530

    page4 style completed

--- gsap-animation.js
+++ gsap-animation.js
@@ -1,2 +1,25 @@
 import gsap from 'gsap';
-console.log(gsap)
+
+document.querySelectorAll('.cards').forEach(card => {
+    let card_logo = card.querySelector('.cards-logo');
+    let car_name = card.querySelector('.car-name');
+    let container = card.querySelector('.hover-after');
+  
+
+    container.addEventListener('mouseenter', (events) => {
+ 
+        gsap.to([card_logo, car_name], {
+            x: 30,
+            opacity: 1,
+            duration: 1
+        });
+    });
+
+    container.addEventListener('mouseleave', () => {
+        gsap.to([card_logo, car_name], {
+            x: -20,
+            opacity: 0,
+            duration: 1
+        });
+    });
+});
